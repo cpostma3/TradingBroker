ff39eb9d5321139f93a0b80c88a11772e6da2fb4
44jon <44jon@192.168.2.120>
2022-04-01 00:30:50 -0400

    Completed Get User

--- TradingBroker/cryptoTrader/src/main/java/authentication/Authentication.java
+++ TradingBroker/cryptoTrader/src/main/java/authentication/Authentication.java
@@ -1,16 +1,17 @@
 package authentication;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-/*
- * 
- */
-
-/* 
- * 
+/* Name: Authentication
+ * Date: April 1
+ * Authors:
+ * Description:
+ *
  */
 public class Authentication {
 
-	/*
-	 * Verifies if the username and password are correct. 
+	/* Verifies if the username and password are correct. 
 	 * Returns True if they match and false if there is no user or the password is incorrect
 	 * Arguments: 
 	 * 	username: a string to represent the user 
@@ -22,24 +23,46 @@ public class Authentication {
 		return false;
 	}
 	
-	//
+	/* a method to write a new user to the document 
+	 * returns true if it works false if user already exists
+	 */
 	public boolean addUser(String username, String password) {
 		return false;
 	}
 	
 	
-	/* a helper method to GetUser takes a string usern
-	 * 
+	/* getUser is a helper method to GetUser a password of a user in the 
+	 * database. If no user exists it will return null
+	 * TODO
 	 */
-	private String getUser(String username) {
+	public static String getUser(String username) {
+		//Instantiate Scanner
+		Scanner scan;
+		try {
+			scan = new Scanner(new File("Accounts.txt"));
+		//Remove blank line at start
+		String next = scan.nextLine();
 		
-		 //TODO
+		//Comb through document for the correct username 
+		while(scan.hasNextLine()) {
+			//Get the next username
+			next = scan.nextLine();
+
+			if(next.equals(username))
+				return scan.nextLine();
+			//Brush past next password
+			next = scan.nextLine();			
+		}
+		
+		return null;		 
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
-	/* a helper method to write a new user to the document 
-	 * returns true if it works false if user already exists
-	 */
-	private boolean setUser(String username, String password) {
-		return false;
-	}
+	
+	
 }
