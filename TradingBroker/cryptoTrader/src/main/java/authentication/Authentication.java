38aa42370fa4fc1d627db91f4816c04d1d9a0f7b
44jon <44jon@192.168.2.120>
2022-03-31 22:39:35 -0400

    Minor work on authentication

--- TradingBroker/cryptoTrader/src/main/java/authentication/Authentication.java
+++ TradingBroker/cryptoTrader/src/main/java/authentication/Authentication.java
@@ -1,5 +1,36 @@
 package authentication;
 
+/*
+ * 
+ */
+
+/* 
+ * 
+ */
 public class Authentication {
 
+	public boolean login(String username, String password) {
+		return false;
+	}
+	
+	//
+	public boolean addUser(String username, String password) {
+		return false;
+	}
+	
+	
+	/* a helper method to GetUser takes a string usern
+	 * 
+	 */
+	private String getUser(String username) {
+		return "";
+		 //TODO
+	}
+	
+	/* a helper method to write a new user to the document 
+	 * returns true if it works false if user already exists
+	 */
+	private boolean setUser(String username, String password) {
+		return false;
+	}
 }
